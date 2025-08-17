3a94317c8c4b77edac2c57636b16398329984360
Robin <Lvbrobinhood@gmail.com>
2025-08-17 21:21:24 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Batman.java
@@ -0,0 +1,11 @@
+public class Batman {
+    public static void main(String[] args) {
+        String line = "_____________________________________________________\n";
+        System.out.println( line
+                + "Hello! I'm Batman.\n"
+                + "What can I do for you?\n"
+                + line
+                + "Bye. Hope to see you again soon!\n"
+                + line);
+    }
+}
