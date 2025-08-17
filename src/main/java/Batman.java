e3cb15d7d32f81e0ff5872166e2ef1f307122bd1
Robin <Lvbrobinhood@gmail.com>
2025-08-17 21:40:40 +0800

    Echo

--- src/main/java/Batman.java
+++ src/main/java/Batman.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class Batman {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String line = "_____________________________________________________\n";
-        System.out.println( line
-                + "Hello! I'm Batman.\n"
-                + "What can I do for you?\n"
-                + line
-                + "Bye. Hope to see you again soon!\n"
-                + line);
+        System.out.println( line + "Hello! I'm Batman.\n" + "What can I do for you?\n" + line);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
+                break;
+            } else {
+                System.out.println(line + input + "\n" + line);
+            }
+        }
+
+        sc.close();
     }
 }
